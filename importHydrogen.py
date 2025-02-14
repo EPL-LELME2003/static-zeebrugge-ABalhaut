94437c330d31f2803ff098387982e34c3aacb839
ABalhaut <augustin.balhaut@student.uclouvain.be>
2025-02-14 15:49:43 +0100

    First try

--- importHydrogen.py
+++ importHydrogen.py
@@ -5,24 +5,42 @@ import pyomo.environ as pyo
 model = pyo.ConcreteModel()
 
 # Define model parameters
-##########################################
-############ CODE TO ADD HERE ############
-##########################################
+Vboat = 200000
+LHV_CH4 = 50 * 10**6
+rho_CH4 = 500
+losses_CH4 = 0.35
+H2t_CH4 = 0.25
+
+LHV_NH3 = 18.5 * 10**6
+rho_NH3 = 600
+losses_NH3 = 0.4
+H2t_NH3 = 0.18
+
+CO2CH4 = 2.75
 
 # Define model variables
-##########################################
-############ CODE TO ADD HERE ############
-##########################################
+model.boatsCH4 = pyo.Var(within = pyo.NonNegativeReals, bounds =(0,100)) 
+model.boatsNH3 = pyo.Var(within = pyo.NonNegativeReals, bounds =(0,100))
+
 
 # Define the objective functions
-##########################################
-############ CODE TO ADD HERE ############
-##########################################
+model.obj = pyo.Objective(expr = Vboat*rho_CH4*H2t_CH4*model.boatsCH4 + Vboat*rho_NH3*H2t_NH3*model.boatsNH3, sense = pyo.maximize)
+
+
 
 # Define the constraints
-##########################################
-############ CODE TO ADD HERE ############
-##########################################
+def con_rule1(model):
+    return model.boatsCH4 + model.boatsNH3 <= 100
+model.con = pyo.Constraint(rule = con_rule1)
+
+def con_rule2(model):
+    return Vboat*rho_CH4*model.boatsCH4*CO2CH4 <= 14*10**6
+model.con2 = pyo.Constraint(rule = con_rule2)
+
+def con_rule3(model):
+    return Vboat*rho_CH4*model.boatCH4*LHV_CH4/(1-losses_CH4) + Vboat*rho_NH3*model.boatsNH3*LHV_NH3/(1-losses_NH3) <= 140*10**12
+model.con3 = pyo.Constraint(rule = con_rule3)
+
 
 # Specify the path towards your solver (gurobi) file
 solver = pyo.SolverFactory('...')
